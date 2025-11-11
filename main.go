310a6450f545e96d8344fcfbd275b669e1e800ca
Joshua Luacs <joshuamlucas91@gmail.com>
2025-11-10 19:53:39 -0500

    feat: update main to impl read write logic for config

--- main.go
+++ main.go
@@ -1,6 +1,33 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"github.com/Joshua-Lucas/blog-aggregator/internal"
+	"os"
+)
 
 func main() {
+	config, err := Config.Read()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	err = config.SetUser("Josh")
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	config, err = Config.Read()
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%v", config)
+
 }
