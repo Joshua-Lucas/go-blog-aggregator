aef2d1d6acdba4157a03c5f361868a579f21530e
Joshua Luacs <joshuamlucas91@gmail.com>
2025-11-17 08:33:41 -0500

    feat: add the commands module

---
+++ internal/commands/commands.go
@@ -0,0 +1,58 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/Joshua-Lucas/blog-aggregator/internal/config"
+)
+
+type State struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	v, ok := c.handlers[cmd.name]
+
+	if !ok {
+		return fmt.Errorf("There is no handler for the provided command")
+	}
+
+	err := v(s, cmd)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlers[name] = f
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) <= 0 {
+		return fmt.Errorf("Login in handler expects a single argument, the username")
+	}
+
+	userName := cmd.args[0]
+
+	err := s.cfg.SetUser(userName)
+
+	if err != nil {
+		return err
+	}
+
+	println("User has been set")
+
+	return nil
+}
