28b3d5d16e6110d7271b36eb74d6d78ae32f8e48
Joshua Luacs <joshuamlucas91@gmail.com>
2025-11-17 08:32:33 -0500

    refactor: mv config module into a confg directory

--- main.go
+++ main.go
@@ -2,32 +2,32 @@ package main
 
 import (
 	"fmt"
-	"github.com/Joshua-Lucas/blog-aggregator/internal"
+	"github.com/Joshua-Lucas/blog-aggregator/internal/config"
 	"os"
 )
 
 func main() {
-	config, err := Config.Read()
+	cfg, err := config.Read()
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 
-	err = config.SetUser("Josh")
+	err = cfg.SetUser("Josh")
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 
-	config, err = Config.Read()
+	cfg, err = config.Read()
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("%v", config)
+	fmt.Printf("%v", cfg)
 
 }
