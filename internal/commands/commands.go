a6b5483e49141f4bb71dcc7297c720e8efb2afb2
Joshua Luacs <joshuamlucas91@gmail.com>
2025-11-17 08:35:45 -0500

    fix: state struct to public

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -16,10 +16,10 @@ type command struct {
 }
 
 type commands struct {
-	handlers map[string]func(*state, command) error
+	handlers map[string]func(*State, command) error
 }
 
-func (c *commands) run(s *state, cmd command) error {
+func (c *commands) run(s *State, cmd command) error {
 	v, ok := c.handlers[cmd.name]
 
 	if !ok {
@@ -35,11 +35,11 @@ func (c *commands) run(s *state, cmd command) error {
 	return nil
 }
 
-func (c *commands) register(name string, f func(*state, command) error) {
+func (c *commands) register(name string, f func(*State, command) error) {
 	c.handlers[name] = f
 }
 
-func handlerLogin(s *state, cmd command) error {
+func handlerLogin(s *State, cmd command) error {
 	if len(cmd.args) <= 0 {
 		return fmt.Errorf("Login in handler expects a single argument, the username")
 	}
