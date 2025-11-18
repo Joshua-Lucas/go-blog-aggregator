650174836ce2d5e9df080bd8af9451cc3bb68813
Joshua Luacs <joshuamlucas91@gmail.com>
2025-11-18 08:25:48 -0500

    refactor: update main to set command, capture user input and run cmds

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/Joshua-Lucas/blog-aggregator/internal/config"
 	"os"
+	"github.com/Joshua-Lucas/blog-aggregator/internal/commands"
+	"github.com/Joshua-Lucas/blog-aggregator/internal/config"
 )
 
 func main() {
@@ -14,20 +15,34 @@ func main() {
 		os.Exit(1)
 	}
 
-	err = cfg.SetUser("Josh")
+	state := commands.State{Config: &cfg}	
+
+	cmd := commands.Commands{
+		Handlers: make(map[string]func(*commands.State, commands.Command) error),
+	}
+
+	cmd.Register("login", commands.HandlerLogin)
+
+	fmt.Println(cmd)
+
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "error: There are less than two arguments.\n")
+		os.Exit(1)
+	}
+
+	userArg := commands.Command {
+		Name: os.Args[1],
+		Args: []string{os.Args[2]},
+	}
+
+	//TODO: handle if there is no second arg passesd and send an error
+
+	err = cmd.Run(&state, userArg)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 
-	cfg, err = config.Read()
-
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("%v", cfg)
-
+	fmt.Printf("%#v\n", cfg)
 }
